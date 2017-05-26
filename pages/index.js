d1c297053b439692b9e54327bb598f76302f75c9
dqisme <dqisme@outlook.com>
2017-05-26 15:25:25 +0800

    [dq] #1 add basic two inputs to practice word and its meaning

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,102 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { deepOrange500 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import { TextField } from 'material-ui'
 
-class Index extends Component {
-    render() {
-        return (
-            <div>
-                <p>Word Fight!</p>
-            </div>
-        );
+// Make sure react-tap-event-plugin only gets injected once
+// Needed for material-ui
+if (!process.tapEventInjected) {
+    injectTapEventPlugin()
+    process.tapEventInjected = true
+}
+
+const styles = {
+    container: {
+        textAlign: 'center',
+        paddingTop: 200
+    },
+    input: {
+        margin: 20
     }
 }
 
-export default Index;
+const muiTheme = {
+    palette: {
+        accent1Color: deepOrange500
+    }
+}
+
+class Index extends React.Component {
+    static getInitialProps ({ req }) {
+        // Ensures material-ui renders the correct css prefixes server-side
+        let userAgent
+        if (process.browser) {
+            userAgent = navigator.userAgent
+        } else {
+            userAgent = req.headers['user-agent']
+        }
+
+        return { userAgent }
+    }
+
+    constructor (props, context) {
+        super(props, context)
+        this.state = {
+            word: '',
+            meaning: ''
+        }
+    }
+
+    handleWordInputChange = (event) => {
+        this.setState({ word: event.target.value })
+    }
+
+    handleMeaningInputChange = (event) => {
+        this.setState({ meaning: event.target.value })
+    }
+
+    handleWordInputKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            this.setState({ meaning: '' })
+            this.meaningInput.focus()
+        }
+    }
+
+    handleMeaningInputKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            this.setState({ word: '' })
+            this.wordInput.focus()
+        }
+    }
+
+    render () {
+        const { userAgent } = this.props
+
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme({userAgent, ...muiTheme})}>
+                <div style={styles.container}>
+                    <TextField
+                        value={this.state.word}
+                        onChange={this.handleWordInputChange}
+                        ref={(component) => { this.wordInput = component }}
+                        hintText="Word"
+                        style={styles.input}
+                        onKeyDown={this.handleWordInputKeyDown}
+                    />
+                    <TextField
+                        value={this.state.meaning}
+                        onChange={this.handleMeaningInputChange}
+                        ref={(component) => { this.meaningInput = component }}
+                        style={styles.input}
+                        hintText="Meaning"
+                        onKeyDown={this.handleMeaningInputKeyDown}
+                    />
+                </div>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default Index
