6ae50cb3bcd8889897859b6aef8bd9f864dd35d6
dqisme <dqisme@outlook.com>
2017-05-27 18:29:05 +0800

    [dq] #1 add dialog for settings

--- components/settings.js
+++ components/settings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton } from 'material-ui';
+import { Dialog, FlatButton, IconButton } from 'material-ui';
 import { grey500 } from 'material-ui/styles/colors';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
 
@@ -14,15 +14,51 @@ const styles = {
 class Settings extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {};
+    this.state = {
+      isOpen: false,
+    };
   }
+
+  handleOpen = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  actions = [
+    <FlatButton
+      label="Cancel"
+      primary
+      onTouchTap={this.handleClose}
+    />,
+    <FlatButton
+      label="Submit"
+      primary
+      onTouchTap={this.handleClose}
+    />,
+  ];
 
   render() {
     return (
       <div>
-        <IconButton style={styles.settingButton}>
+        <IconButton onTouchTap={this.handleOpen} style={styles.settingButton}>
           <SettingsIcon color={grey500} />
         </IconButton>
+        <Dialog
+          title="Dialog With Actions"
+          actions={this.actions}
+          modal={false}
+          open={this.state.isOpen}
+          onRequestClose={this.handleClose}
+        >
+          The actions in this window were passed in as an array of React objects.
+        </Dialog>
       </div>
     );
   }
