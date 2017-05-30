d76c3d898a3dc81bdee45d5d2857213884082617
dqisme <dqisme@outlook.com>
2017-05-30 09:13:28 +0800

    [dq] #2 add express server to launch app

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-/**
- * Created by dq on 30/05/2017.
- */
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('/translate', (req, res) => {
+      return res.send(JSON.stringify({ foo: 'bar' }));
+    });
+
+    server.get('/b', (req, res) => {
+      return app.render(req, res, '/a', req.query)
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000')
+    });
+  });
