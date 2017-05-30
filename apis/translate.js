8c0d47c590cbaf8fd6b065167c0169326bd7c672
dqisme <dqisme@outlook.com>
2017-05-30 11:41:24 +0800

    [dq] #3 make basic translate service api using google cloud

--- apis/translate.js
+++ apis/translate.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const googleTranslateService = require('@google-cloud/translate');
+const path = require('path');
 
-router.get('/', function (req, res) {
-  res.send('this is translate api');
+const googleTranslateInstance = googleTranslateService({
+  projectId: 'zippy-acronym-169116',
+  keyFilename: path.resolve(__dirname, 'translate_service_key.json'),
+});
+
+router.get('/:word', function (req, res) {
+  const word = req.params.word;
+  if (word) {
+    googleTranslateInstance.translate(word, 'zh', (error, translation) => {
+      if (!error) {
+        res.send({ translation });
+      } else {
+        res.send({ error: 'translation service error' });
+        console.error(error);
+      }
+    });
+  } else {
+    res.send({ error: 'invalid word' });
+  }
 });
 
 module.exports = router;
